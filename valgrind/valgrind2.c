9d67b144f4f2f7fdbcda9ee6d3f4307bb816f6f1
Daniele Visentin <daniele.visentin.3@studenti.unipd.it>
2024-05-19 09:39:12 +0000

    aggiunto free() per le malloc() in cui mancava

--- valgrind/valgrind2.c
+++ valgrind/valgrind2.c
@@ -3,16 +3,17 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+// la memoria va liberata ogni volta quindi ho messo 2 free in più
 
 int main()
 {
 	char *p;
 
 	p = (char *) malloc(19);
-
+	free(p);
 	p = (char *) malloc(12);
 	free(p);
-
-	p = (char *) malloc(16);
+	p = (char *) malloc(16);	
+	free(p);
 	return 0;
 }
