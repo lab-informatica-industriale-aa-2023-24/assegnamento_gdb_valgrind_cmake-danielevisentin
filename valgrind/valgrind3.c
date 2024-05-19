e8d14f3f8d54a4c215360e7dd8a0221b5298dee2
Daniele Visentin <daniele.visentin.3@studenti.unipd.it>
2024-05-19 09:52:28 +0000

    modificato il secondo ciclo for e reimpostato s in modo tale che punti al primo elemento di p prima di entrare nel secondo for e aggiunto il free() di p

--- valgrind/valgrind3.c
+++ valgrind/valgrind3.c
@@ -9,14 +9,18 @@ int main()
 	p = (char *) malloc(20);
 
 	char *s = p;
+	
 	for(int n = 0; n < 20; n++) {
 		*s = '\0';
 		s++;
 	}
-
-	for(int n = 0; n < 30; n++) {
+	s = p;
+	for(int n = 0; n < 20; n++) {
+		
 		*s = 'A';
 		s++;
 	}
+	
+	free(p);
 
 }
